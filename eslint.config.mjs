2661e5680343ec32bb55ef32e66757dca81d8c03
Mahdi Al Hasan <webpro.mahdi@gmail.com>
2025-06-15 19:49:00 +0600

    first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import prettierPlugin from 'eslint-plugin-prettier'
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  { languageOptions: { globals: globals.browser } },
+  {
+    rules: {
+      eqeqeq: 'off',
+      'no-unused-vars': 'error',
+      'no-console': 'warn',
+      'no-undef': 'error',
+      'prefer-const': ['error', { ignoreReadBeforeAssign: true }],
+    },
+  },
+  {
+    ignores: ['.node_modules/*', 'dist/*'],
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...prettierPlugin.configs.recommended.rules,
+    },
+  },
+]
